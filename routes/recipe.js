c4ae922f8996769ebd62c4c168af7daa8b2c8f61
ashrafpoless <ashrafpoless@gmail.com>
2024-07-17 01:43:02 +0200

    recipeRoutes

--- routes/recipe.js
+++ routes/recipe.js
@@ -5,5 +5,10 @@ import recipeControllers from '../controllers/recipe.js';
 const router = express.Router();
 
 // routes
+router.get('/',recipeControllers.getAllRecipes);
+router.get('/:id', recipeControllers.getOneRecipe);
+router.post('/', verifyToken, recipeControllers.postRecipe);
+router.put('/:id', verifyToken, recipeControllers.updateRecipe);
+router.delete('/:id', verifyToken, recipeControllers.deleteRecipe);
 
 export default router;
