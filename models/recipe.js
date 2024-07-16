2f617a1addeef34146d7faf5ba395fc7fa84ecc3
ashrafpoless <ashrafpoless@gmail.com>
2024-07-17 01:45:40 +0200

    recipeModels

--- models/recipe.js
+++ models/recipe.js
@@ -2,7 +2,19 @@ import query from '../config/db.js';
 
 const createRecipeTable = async () => {
     try {
-    } catch (err) {}
+        const createTableForRecipe = `
+        CREATE  TABLE IF NOT EXIST recipe(
+        id INT UNIQUE AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(50) NOT NULL,
+        cook VARCHAR(30) NOT NULL,
+        ingredients VARCHAR(100) NOT NULL,
+        description VARCHAR(200) NOT NULL,
+        img VARCHAR(30) NOT NULL
+        )`
+        const createTable = await query(createTableForRecipe)
+    } catch (err) {
+        console.error(err);
+    }
 };
 
 export default createRecipeTable;
